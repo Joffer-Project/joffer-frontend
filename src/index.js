49d0c582a4c111f6aafa2c13f6606380a6bc6932
Sushan <t2masu00@students.oamk.fi>
2024-04-10 00:28:23 +0300

    added talent base layout elements

--- src/index.js
+++ src/index.js
@@ -1,35 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import ErrorPage from "./pages/error-page";
 import App from "./App";
+import BackgroundCarousel from "../src/pages/talent/tlogin/tlogin.jsx";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "talent",
-    element: <div>our talent program!</div>,
-  },
-  {
-    path: "talent/signup",
-    element: <div>Sign up for our talent program!</div>,
-  },
-  {
-    path: "recruiter",
-    element: <div>Sign up for our talent program!</div>,
-  },
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "talent",
+		element: <div>our talent program!</div>,
+	},
+	{
+		path: "talent/signup",
+		element: <BackgroundCarousel />,
+	},
+	{
+		path: "recruiter",
+		element: <div>Sign up for our talent program!</div>,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
