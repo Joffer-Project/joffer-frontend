65ebef8d7d1a65d67687af09bf16e0cd82925e2f
Hakan Asmaoglu <c2asha00@students.oamk.fi>
2024-04-09 15:04:02 +0300

    Some updates

--- src/pages/landing/landingpage.jsx
+++ src/pages/landing/landingpage.jsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import "../../../src/App.css";
+import recruiterButtonImg from "../../../src/images/login-screen/login-screen-recruiter-button-bg.webp";
+import talentButtonImg from "../../../src/images/login-screen/login-screen-talent-button-bg (2).webp";
 import logoSlogan from "../../../src/images/logo-slogan.png";
+
 import "./landpage.css";
 const TalentSection = () => {
   return (
     <section className="talent-section">
       <div className="talent-content">
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/349182bd2c56ab027023d979c48e3b86953b07cec1a23a8633c83a43a7a32040?apiKey=d5d118b7190b41549398dbbdd251edfc&"
-          alt=""
-          className="talent-background"
-        />
+        <img class="logo" src={talentButtonImg} alt="Logo" />
         <div className="talent-text">
           <h2 className="talent-title">Talent</h2>
           <p className="talent-description">
@@ -26,11 +25,7 @@ const RecruiterSection = () => {
   return (
     <section className="recruiter-section">
       <div className="recruiter-content">
-        <img
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/ec64a07570cf11a760cf05b9a029f1cf141fa05bb5027d9a83814da73b14446c?apiKey=d5d118b7190b41549398dbbdd251edfc&"
-          alt=""
-          className="recruiter-background"
-        />
+        <img class="logo" src={recruiterButtonImg} alt="Logo" />
         <div className="recruiter-text">
           <h2 className="recruiter-title">Recruiter</h2>
           <p className="recruiter-description">
