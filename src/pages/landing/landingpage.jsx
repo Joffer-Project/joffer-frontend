205eb6cc9ded94f2c4bd110cdc100ee15e39fe21
Hakan Asmaoglu <c2asha00@students.oamk.fi>
2024-04-10 13:12:21 +0300

    Refactored file names and images added

--- src/pages/landing/landingpage.jsx
+++ src/pages/landing/landingpage.jsx
@@ -1,85 +1,82 @@
 import * as React from "react";
 import { useState } from "react";
 import "../../../src/App.css";
-import recruiterBg from "../../../src/images/login-screen/login-screen-recruiter-button-bg.webp";
-import talentBg from "../../../src/images/login-screen/login-screen-talent-button-bg.webp";
-import logoSlogan from "../../../src/images/logo-slogan.png";
+import recruiterBg from "../../../src/images/landing-page/recruiter-bg.webp";
+import talentBg from "../../../src/images/landing-page/talent-bg.webp";
+import logoSlogan from "../../../src/images/logo-slogan.svg";
 
 import "./landpage.css";
 
 const TalentSection = () => {
-	// State to track if the button is hovered
-	const [isHovered, setIsHovered] = useState(false);
+  // State to track if the button is hovered
+  const [isHovered, setIsHovered] = useState(false);
 
-	return (
-		<section className={`talent-section ${isHovered ? "shadow" : ""}`}>
-			<div className="talent-content">
-				<div className="talent-text">
-					<button
-						className="talent-btn"
-						onMouseEnter={() => setIsHovered(true)}
-						onMouseLeave={() => setIsHovered(false)}>
-						Talent
-					</button>
-					<p
-						className={`talent-description ${
-							isHovered ? "darker" : ""
-						}`}>
-						For the ones who seek new job opportunities!
-					</p>
-				</div>
-			</div>
-			<img
-				className={`talent-bg ${isHovered ? "hovered" : ""}`}
-				src={talentBg}
-				alt=""
-			/>
-		</section>
-	);
+  return (
+    <section className={`talent-section ${isHovered ? "shadow" : ""}`}>
+      <div className="talent-content">
+        <div className="talent-text">
+          <button
+            className="talent-btn"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Talent
+          </button>
+          <p className={`talent-description ${isHovered ? "darker" : ""}`}>
+            For the ones who seek new job opportunities!
+          </p>
+        </div>
+      </div>
+      <img
+        className={`talent-bg ${isHovered ? "hovered" : ""}`}
+        src={talentBg}
+        alt=""
+      />
+    </section>
+  );
 };
 
 const RecruiterSection = () => {
-	// State to track if the button is hovered
-	const [isHovered, setIsHovered] = useState(false);
+  // State to track if the button is hovered
+  const [isHovered, setIsHovered] = useState(false);
 
-	return (
-		<section className={`recruiter-section ${isHovered ? "shadow" : ""}`}>
-			<div className="recruiter-content">
-				<div className="recruiter-text">
-					<button
-						className="recruiter-btn"
-						onMouseEnter={() => setIsHovered(true)}
-						onMouseLeave={() => setIsHovered(false)}>
-						Recruiter
-					</button>
-					<p
-						className={`recruiter-description ${
-							isHovered ? "darker" : ""
-						}`}>
-						For the ones who seek new talents to work with!
-					</p>
-				</div>
-			</div>
+  return (
+    <section className={`recruiter-section ${isHovered ? "shadow" : ""}`}>
+      <div className="recruiter-content">
+        <div className="recruiter-text">
+          <button
+            className="recruiter-btn"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            Recruiter
+          </button>
+          <p className={`recruiter-description ${isHovered ? "darker" : ""}`}>
+            For the ones who seek new talents to work with!
+          </p>
+        </div>
+      </div>
 
-			<img
-				className={`recruiter-bg ${isHovered ? "hovered" : ""}`}
-				src={recruiterBg}
-				alt=""
-			/>
-		</section>
-	);
+      <img
+        className={`recruiter-bg ${isHovered ? "hovered" : ""}`}
+        src={recruiterBg}
+        alt=""
+      />
+    </section>
+  );
 };
 
 function MyComponent() {
-	return (
-		<main className="main-container">
-			<img class="logo" src={logoSlogan} alt="Logo" />
-			<div className="sections-container">
-				<TalentSection />
-				<RecruiterSection />
-			</div>
-		</main>
-	);
+  return (
+    <main className="main-container">
+      <img className="logo" src={logoSlogan} alt="Logo" />
+      <h1 className="slogan">Your professional matchmaker!</h1>
+      <div className="sections-container">
+        <TalentSection />
+        <RecruiterSection />
+      </div>
+    </main>
+  );
 }
 
 export default MyComponent();
