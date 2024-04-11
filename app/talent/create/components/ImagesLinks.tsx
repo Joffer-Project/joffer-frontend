f964b7fd22688a375759105412ba972624a3c21d
Shovan Das <shovondas26@gmail.com>
2024-04-11 08:46:32 +0300

    design update in image and link step

--- app/talent/create/components/ImagesLinks.tsx
+++ app/talent/create/components/ImagesLinks.tsx
@@ -9,6 +9,22 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Clipboard, Copy, Dribbble, Github, Linkedin, UserCircle2 } from "lucide-react";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { cn } from "@/lib/utils";
+import Medium from "@/public/icons/Medium";
 
 interface ImagesLinksProps {
     form: any;
@@ -20,12 +36,33 @@ const ImagesLinks: React.FC<ImagesLinksProps> = ({
 }) => {
 
 
+    const [currentBox, setCurrentBox] = React.useState("");
+    const [currentLink, setCurrentLink] = React.useState("");
+    const [githubLink, setGithubLink] = React.useState("");
+    const [linkedinLink, setLinkedinLink] = React.useState("");
+    const [mediumLink, setMediumLink] = React.useState("");
+    const [dribbleLink, setDribbleLink] = React.useState("");
+    const [personalLink, setPersonalLink] = React.useState("");
+
+    const saveLink = () => {
+        if (currentBox === "github") {
+            setGithubLink(currentLink);
+        } else if (currentBox === "linkedin") {
+            setLinkedinLink(currentLink);
+        } else if (currentBox === "medium") {
+            setMediumLink(currentLink);
+        } else if (currentBox === "dribble") {
+            setDribbleLink(currentLink);
+        } else if (currentBox === "personal") {
+            setPersonalLink(currentLink);
+        }
+    }
 
     return (
-        <>
+        <><Dialog>
             <h2 className="font-bold text-3xl mb-4 text-[#FF7E33]">Images & Links</h2>
             <p className="font-medium text-xl mb-6">The images and links that you added will be shown on your profile as listed below. You can always update your images in the settings.</p>
-            <div className="flex flex-col gap-8">
+            <div className="flex flex-col gap-2">
                 <div className="flex gap-2">
                     <FormField
                         control={form.control}
@@ -38,7 +75,119 @@ const ImagesLinks: React.FC<ImagesLinksProps> = ({
                                         onChange={(url) => field.onChange(url)}
                                         onRemove={() => field.onChange('')}
                                         label="Profile Picture"
-                                        />
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="flex gap-2 -mb-4">
+                    <FormField
+                        control={form.control}
+                        name=""
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            className={cn("w-[80px] h-[80px] border-2 flex flex-col items-center justify-center gap-2", githubLink ? "border-[#FF7E33]" : "border-gray-600")}
+                                            onClick={() => setCurrentBox("github")}
+                                        >
+                                            <Github className={cn("h-8 w-8", githubLink ? "text-[#FF7E33]" : "text-gray-600")} />
+                                            <small>Github</small>
+                                        </Button>
+                                    </DialogTrigger>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name=""
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            className={cn("w-[80px] h-[80px] border-2 flex flex-col items-center justify-center gap-2", linkedinLink ? "border-[#FF7E33]" : "border-gray-600")}
+                                            onClick={() => setCurrentBox("linkedin")}
+                                        >
+                                            <Linkedin className={cn("h-8 w-8", linkedinLink ? "text-[#FF7E33]" : "text-gray-600")} />
+                                            <small>LinkedIn</small>
+                                        </Button>
+                                    </DialogTrigger>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name=""
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            className={cn("w-[80px] h-[80px] border-2 flex flex-col items-center justify-center gap-2", mediumLink ? "border-[#FF7E33]" : "border-gray-600")}
+                                            onClick={() => setCurrentBox("medium")}
+                                        >
+                                            <Medium className={cn("h-8 w-8", mediumLink ? "text-[#FF7E33]" : "text-gray-600")} />
+                                            <small>Medium</small>
+                                        </Button>
+                                    </DialogTrigger>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name=""
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            className={cn("w-[80px] h-[80px] border-2 flex flex-col items-center justify-center gap-2", dribbleLink ? "border-[#FF7E33]" : "border-gray-600")}
+                                            onClick={() => setCurrentBox("dribble")}
+                                        >
+                                            <Dribbble className={cn("h-8 w-8", dribbleLink ? "text-[#FF7E33]" : "text-gray-600")} />
+                                            <small>Dribble</small>
+                                        </Button>
+                                    </DialogTrigger>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name=""
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormControl>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            type="button"
+                                            variant="secondary"
+                                            className={cn("w-[80px] h-[80px] border-2 flex flex-col items-center justify-center gap-2", personalLink ? "border-[#FF7E33]" : "border-gray-600")}
+                                            onClick={() => setCurrentBox("personal")}
+                                        >
+                                            <UserCircle2 className={cn("h-8 w-8", personalLink ? "text-[#FF7E33]" : "text-gray-600")} />
+                                            <small>Personal</small>
+                                        </Button>
+                                    </DialogTrigger>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -46,6 +195,43 @@ const ImagesLinks: React.FC<ImagesLinksProps> = ({
                     />
                 </div>
             </div>
+
+            <DialogContent className="sm:max-w-md">
+                <DialogHeader>
+                    <DialogTitle>Share link</DialogTitle>
+                    <DialogDescription>
+                        Anyone who has this link will be able to view this.
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="flex items-center space-x-2">
+                    <div className="grid flex-1 gap-2">
+                        <Label htmlFor="link" className="sr-only">
+                            Link
+                        </Label>
+                        <Input
+                            id="link"
+                            type="text"
+                            value={currentLink} onChange={(e) => setCurrentLink(e.target.value)}
+                            placeholder="Paste link here"
+                            className="w-full"
+
+                        />
+                    </div>
+                    <Button type="submit" size="sm" className="px-3">
+                        <span className="sr-only">Paste</span>
+                        <Clipboard className="h-4 w-4" onClick={() => navigator.clipboard.readText().then((clipText) => setCurrentLink(clipText))} />
+                    </Button>
+                </div>
+                <DialogFooter className="sm:justify-start">
+
+                    <DialogClose asChild>
+                        <Button type="button" variant="default" onClick={saveLink}>
+                            Save
+                        </Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
         </>
     );
 };
