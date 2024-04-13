78f4cb3e9fc418fca861eb3eba1c063e676048ed
Hakan Asmaoglu <c2asha00@students.oamk.fi>
2024-04-13 17:24:18 +0300

    Final step(not responsive) styling 🎨

--- src/pages/talent/signup/components/AboutInfo.jsx
+++ src/pages/talent/signup/components/AboutInfo.jsx
@@ -2,16 +2,23 @@ import React from "react";
 import "../styles/AboutInfo.css";
 
 const AboutInfo = ({ form }) => {
-    return (
-        <>
-            <h2 className="ai-heading ai-bold ai-text3xl ai-orange">Final step</h2>
-            <p className="ai-medium ai-textxl ai-mb6">Share some information about your career path. Remember to keep it direct, these pieces of information can place you ahead.</p>
-            <div className="ai-formGroup">
-                <label htmlFor="description" className="ai-block ai-mb2">Tell us a little bit about yourself</label>
-                <textarea id="description" {...form.register("description")} placeholder="Tell us a little bit about yourself" className="ai-inputField"></textarea>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <h2 className="ai-title">Final step</h2>
+      <p className="ai-subtitle">
+        Share some information about your career path. Remember to keep it
+        direct, these pieces of information can place you ahead.
+      </p>
+      <div className="ai-formGroup">
+        <textarea
+          id="description"
+          {...form.register("description")}
+          placeholder="Click to start typing..."
+          className="ai-inputField"
+        ></textarea>
+      </div>
+    </>
+  );
 };
 
 export default AboutInfo;
