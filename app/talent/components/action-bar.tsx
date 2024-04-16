5e425efc94b7a60758e6cb8cfbd82ade689812c6
Shovan Das <shovondas26@gmail.com>
2024-04-17 01:52:07 +0300

    action bar responsive

--- app/talent/components/action-bar.tsx
+++ app/talent/components/action-bar.tsx
@@ -10,40 +10,40 @@ export default function ActionBar() {
                 isOpen={open}
                 onClose={() => setOpen(false)}
             />
-        <div className="flex justify-between items-center flex-wrap gap-2 py-8">
+        <div className="flex justify-between items-center gap-2 py-8">
             <div className="flex flex-col justify-center items-center gap-1">
                 <img
                     src="/images/question-mark.png"
                     alt="logo"
                     onClick={() => setOpen(true)}
-                    className={cn("h-12 w-auto cursor-pointer")}
+                    className={cn("h-8 md:h-10 lg:h-12 w-auto cursor-pointer")}
                 />
-                <div className="text-[18px] font-medium text-[$A3A3A3]">How it works?</div>
+                <div className="text-[14px] sm:text-[16] lg:text-[18px] font-medium text-[$A3A3A3]">How it works?</div>
             </div>
             <div className="flex justify-center items-end gap-14">
                 <img
                     src="/images/Close.png"
                     alt="logo"
-                    className={cn("h-20 w-auto")}
+                    className={cn("h-12 md:h-16 lg:h-20 w-auto cursor-pointer transition ease-in-out delay-150 hover:-translate-y-2 hover:scale-110 duration-200")}
                 />
                 <img
                     src="/images/Superlike.png"
                     alt="logo"
-                    className={cn("h-36 w-auto")}
+                    className={cn("h-20 md:h-24 lg:h-36 w-auto cursor-pointer transition ease-in-out delay-150 hover:-translate-y-2 hover:scale-110 duration-200")}
                 />
                 <img
                     src="/images/Like.png"
                     alt="logo"
-                    className={cn("h-20 w-auto")}
+                    className={cn("h-12 md:h-16 lg:h-20 w-auto cursor-pointer transition ease-in-out delay-150 hover:-translate-y-2 hover:scale-110 duration-200")}
                 />
             </div>
             <div className="flex flex-col justify-center items-center gap-1">
                 <img
                     src="/images/settings.png"
                     alt="logo"
-                    className={cn("h-12 w-auto")}
+                    className={cn("h-8 md:h-10 lg:h-12 w-auto cursor-pointer")}
                 />
-                <div className="text-[18px] font-medium text-[$A3A3A3]">Settings</div>
+                <div className="text-[14px] sm:text-[16] lg:text-[18px] font-medium text-[$A3A3A3]">Settings</div>
             </div>
         </div>
         </>
