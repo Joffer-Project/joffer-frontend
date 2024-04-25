0eff8553cb04c0b48d554a59724c345a52934d72
Hakan Asmaoglu <c2asha00@students.oamk.fi>
2024-04-25 11:08:35 +0200

    Accordion off ✅

--- app/talent/components/about-company.tsx
+++ app/talent/components/about-company.tsx
@@ -1,32 +1,19 @@
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion";
 import { Company } from '@/types';
 import React from 'react';
+
 interface AboutCompanyProps {
     data: Company | undefined;
 }
 
 const AboutCompany: React.FC<AboutCompanyProps> = ({ data }) => {
     return (
-        <div className="bg-[#FDFDFD] rounded-sm px-4 h-full">
-
-
-            <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                    <AccordionTrigger><h1 className="text-[#616161] text-[16px] font-medium">About {data?.name ? data.name : "Company"}</h1></AccordionTrigger>
-                    <AccordionContent>
-                        <div className="overflow-y-auto max-h-[100px] text-[14px] text-[#7F7F7F]">
-                        How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.
-                        How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.How are you hakan, this job is only for you.
-                            {data?.description ? data.description : "No data available"}
-                        </div>
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
+        <div className="bg-[#FDFDFD] rounded-sm p-4 h-full xls:h-full">
+            <div className="mb-1">
+                <h1 className="text-[#616161] text-[16px] font-medium">About Nokia {data?.name ? data.name : ""}</h1>
+            </div>
+            <div className="overflow-y-auto max-h-[300px] text-[14px] text-[#7F7F7F]">
+                {data?.description ? data.description : "No data available"}
+            </div>
         </div>
     );
 }
